fa7604867b5d34368caebf0f397083ffd23ac090
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2024-01-05 00:18:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let textParagraph = document.getElementById('text');
+  
+  
+    textParagraph.textContent = "This is really cool!";
+  });
